2dacc1ed56803ebce3abc47e6ee7a547ce2d232c
Ilya Mihaylov <ellepsis07@gmail.com>
2015-10-08 22:52:24 +0300

    Added part of client generating

--- src/main/java/com/ellepsis/databaseGenerator/Application.java
+++ src/main/java/com/ellepsis/databaseGenerator/Application.java
@@ -13,6 +13,8 @@ import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Created by vladislav on 30.09.15.
@@ -21,6 +23,7 @@ import java.net.URISyntaxException;
 public class Application implements CommandLineRunner {
     @Autowired private ClientRepository clientRepository;
     @Autowired private ClientTypeRepository clientTypeRepository;
+    Random random = new Random();
     public static void main(String... args) {
         SpringApplication.run(Application.class, args);
     }
@@ -45,9 +48,33 @@ public class Application implements CommandLineRunner {
     }
 
     public void generateFemaleClients() throws URISyntaxException {
+        List<ClientType> clientTypes = clientTypeRepository.findAll();
+        clientTypes.sort((o1, o2) -> (int) (o1.getId() - o2.getId()));
         RestTemplate restTemplate = new RestTemplate();
         URI uri = new URI("https://www.mockaroo.com/d8c06b60/download?count=1000&key=bfda25a0");
         Client[] clients = restTemplate.getForObject(uri, Client[].class);
-        for(Client client : clients)
+        for(Client client : clients){
+            if (client.getMiddleName() != null)
+            client.setMiddleName(client.getMiddleName().concat("ovna"));
+            client.setGender('F');
+            int rnum = random.nextInt(10);
+
+            if (rnum<1) {
+                client.setClientType(clientTypes.get(0));
+                continue;
+            }
+            if (rnum<5) {
+                client.setClientType(clientTypes.get(1));
+                continue;
+            }
+            if (rnum<7){
+                client.setClientType(clientTypes.get(2));
+                continue;
+            }
+            else {
+                client.setClientType(clientTypes.get(3));
+                continue;
+            }
+        }
     }
 }
--- src/main/java/com/ellepsis/databaseGenerator/Entity/Client.java
+++ src/main/java/com/ellepsis/databaseGenerator/Entity/Client.java
@@ -46,7 +46,7 @@ public class Client extends GenericEntity{
         return middleName;
     }
 
-    public void setMiddleName(String middleName) {
+    public String setMiddleName(String middleName) {
         this.middleName = middleName;
     }
 
