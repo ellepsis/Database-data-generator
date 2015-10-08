a3196767a6781f4fda3df7c04d98758c98d270e1
Ilya Mihaylov <ellepsis07@gmail.com>
2015-10-08 22:21:37 +0300

    Added repositories

--- src/main/java/com/ellepsis/databaseGenerator/Repository/ClientPhoneRepository.java
+++ src/main/java/com/ellepsis/databaseGenerator/Repository/ClientPhoneRepository.java
@@ -1,11 +1,12 @@
 package com.ellepsis.databaseGenerator.Repository;
 
 import com.ellepsis.databaseGenerator.Entity.Client;
+import com.ellepsis.databaseGenerator.Entity.ClientPhone;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * Created by EllepsisRT on 08.10.2015.
  */
-public interface ClientPhoneRepository extends JpaRepository<Client, Long> {
+public interface ClientPhoneRepository extends JpaRepository<ClientPhone, Long> {
     
 }
